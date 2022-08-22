44719e0f8809ca35ee42c784471bb183720a9847
TheLorexxx <104691158+TheLorexxx@users.noreply.github.com>
2022-08-23 00:40:30 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,392 @@
+import time
+import random
+
+steamkod = ["M" , "6" , "8" , "B" , "H" , "Q" , "C" , "2"  , "I" , "A"  , "K" , "Z" , "5" , "0" , "9" , "7" , "3" , "4"  , "P" , "S" , "R" , "T" , "L" , "K"  , "O" , "X" ]
+
+steamkod1 = random.choice(steamkod)
+steamkod2 = random.choice(steamkod)
+steamkod3 = random.choice(steamkod)
+steamkod4 = random.choice(steamkod)
+steamkod5 = random.choice(steamkod)
+
+steamkodbosluk = "-"
+
+steamkod6 = random.choice(steamkod)
+steamkod7 = random.choice(steamkod)
+steamkod8 = random.choice(steamkod)
+steamkod9 = random.choice(steamkod)
+
+steamkodbosluk2 = "-"
+
+steamkod10 = random.choice(steamkod)
+steamkod11 = random.choice(steamkod)
+steamkod12 = random.choice(steamkod)
+steamkod13 = random.choice(steamkod)
+
+# Steam Kod 2
+
+steamkod_a1 = random.choice(steamkod)
+steamkod_a2 = random.choice(steamkod)
+steamkod_a3 = random.choice(steamkod)
+steamkod_a4 = random.choice(steamkod)
+steamkod_a5 = random.choice(steamkod)
+
+steamkod_abosluk = "-"
+
+steamkod_a6 = random.choice(steamkod)
+steamkod_a7 = random.choice(steamkod)
+steamkod_a8 = random.choice(steamkod)
+steamkod_a9 = random.choice(steamkod)
+
+steamkod_abosluk2 = "-"
+
+steamkod_a10 = random.choice(steamkod)
+steamkod_a11 = random.choice(steamkod)
+steamkod_a12 = random.choice(steamkod)
+steamkod_a13 = random.choice(steamkod)
+
+# Steam Kod 3
+
+
+steamkod_b1 = random.choice(steamkod)
+steamkod_b2 = random.choice(steamkod)
+steamkod_b3 = random.choice(steamkod)
+steamkod_b4 = random.choice(steamkod)
+steamkod_b5 = random.choice(steamkod)
+
+steamkod_bbosluk = "-"
+
+steamkod_b6 = random.choice(steamkod)
+steamkod_b7 = random.choice(steamkod)
+steamkod_b8 = random.choice(steamkod)
+steamkod_b9 = random.choice(steamkod)
+
+steamkod_bbosluk2 = "-"
+
+steamkod_b10 = random.choice(steamkod)
+steamkod_b11 = random.choice(steamkod)
+steamkod_b12 = random.choice(steamkod)
+steamkod_b13 = random.choice(steamkod)
+
+# Steam Kod 4
+
+steamkod_c1 = random.choice(steamkod)
+steamkod_c2 = random.choice(steamkod)
+steamkod_c3 = random.choice(steamkod)
+steamkod_c4 = random.choice(steamkod)
+steamkod_c5 = random.choice(steamkod)
+
+steamkod_cbosluk = "-"
+
+steamkod_c6 = random.choice(steamkod)
+steamkod_c7 = random.choice(steamkod)
+steamkod_c8 = random.choice(steamkod)
+steamkod_c9 = random.choice(steamkod)
+
+steamkod_cbosluk2 = "-"
+
+steamkod_c10 = random.choice(steamkod)
+steamkod_c11 = random.choice(steamkod)
+steamkod_c12 = random.choice(steamkod)
+steamkod_c13 = random.choice(steamkod)
+
+# Steam Kod 5
+steamkod_d1 = random.choice(steamkod)
+steamkod_d2 = random.choice(steamkod)
+steamkod_d3 = random.choice(steamkod)
+steamkod_d4 = random.choice(steamkod)
+steamkod_d5 = random.choice(steamkod)
+
+steamkod_dbosluk = "-"
+
+steamkod_d6 = random.choice(steamkod)
+steamkod_d7 = random.choice(steamkod)
+steamkod_d8 = random.choice(steamkod)
+steamkod_d9 = random.choice(steamkod)
+
+steamkod_dbosluk2 = "-"
+
+steamkod_d10 = random.choice(steamkod)
+steamkod_d11 = random.choice(steamkod)
+steamkod_d12 = random.choice(steamkod)
+steamkod_d13 = random.choice(steamkod)
+
+steamkod_d1 = random.choice(steamkod)
+steamkod_d2 = random.choice(steamkod)
+steamkod_d3 = random.choice(steamkod)
+steamkod_d4 = random.choice(steamkod)
+steamkod_d5 = random.choice(steamkod)
+
+steamkod_dbosluk = "-"
+
+steamkod_d6 = random.choice(steamkod)
+steamkod_d7 = random.choice(steamkod)
+steamkod_d8 = random.choice(steamkod)
+steamkod_d9 = random.choice(steamkod)
+
+steamkod_dbosluk2 = "-"
+
+steamkod_d10 = random.choice(steamkod)
+steamkod_d11 = random.choice(steamkod)
+steamkod_d12 = random.choice(steamkod)
+steamkod_d13 = random.choice(steamkod)
+
+# Steam Kod 6
+
+steamkod_e1 = random.choice(steamkod)
+steamkod_e2 = random.choice(steamkod)
+steamkod_e3 = random.choice(steamkod)
+steamkod_e4 = random.choice(steamkod)
+steamkod_e5 = random.choice(steamkod)
+
+steamkod_ebosluk = "-"
+
+steamkod_e6 = random.choice(steamkod)
+steamkod_e7 = random.choice(steamkod)
+steamkod_e8 = random.choice(steamkod)
+steamkod_e9 = random.choice(steamkod)
+
+steamkod_ebosluk2 = "-"
+
+steamkod_e10 = random.choice(steamkod)
+steamkod_e11 = random.choice(steamkod)
+steamkod_e12 = random.choice(steamkod)
+steamkod_e13 = random.choice(steamkod)
+
+steamkod_e1 = random.choice(steamkod)
+steamkod_e2 = random.choice(steamkod)
+steamkod_e3 = random.choice(steamkod)
+steamkod_e4 = random.choice(steamkod)
+steamkod_e5 = random.choice(steamkod)
+
+steamkod_ebosluk = "-"
+
+steamkod_e6 = random.choice(steamkod)
+steamkod_e7 = random.choice(steamkod)
+steamkod_e8 = random.choice(steamkod)
+steamkod_e9 = random.choice(steamkod)
+
+steamkod_ebosluk2 = "-"
+
+steamkod_e10 = random.choice(steamkod)
+steamkod_e11 = random.choice(steamkod)
+steamkod_e12 = random.choice(steamkod)
+steamkod_e13 = random.choice(steamkod)
+
+# Steam Kod 7
+
+steamkod_f1 = random.choice(steamkod)
+steamkod_f2 = random.choice(steamkod)
+steamkod_f3 = random.choice(steamkod)
+steamkod_f4 = random.choice(steamkod)
+steamkod_f5 = random.choice(steamkod)
+
+steamkod_fbosluk = "-"
+
+steamkod_f6 = random.choice(steamkod)
+steamkod_f7 = random.choice(steamkod)
+steamkod_f8 = random.choice(steamkod)
+steamkod_f9 = random.choice(steamkod)
+
+steamkod_fbosluk2 = "-"
+
+steamkod_f10 = random.choice(steamkod)
+steamkod_f11 = random.choice(steamkod)
+steamkod_f12 = random.choice(steamkod)
+steamkod_f13 = random.choice(steamkod)
+
+
+steamkod_f1 = random.choice(steamkod)
+steamkod_f2 = random.choice(steamkod)
+steamkod_f3 = random.choice(steamkod)
+steamkod_f4 = random.choice(steamkod)
+steamkod_f5 = random.choice(steamkod)
+
+steamkod_fbosluk = "-"
+
+steamkod_f6 = random.choice(steamkod)
+steamkod_f7 = random.choice(steamkod)
+steamkod_f8 = random.choice(steamkod)
+steamkod_f9 = random.choice(steamkod)
+
+steamkod_fbosluk2 = "-"
+
+steamkod_f10 = random.choice(steamkod)
+steamkod_f11 = random.choice(steamkod)
+steamkod_f12 = random.choice(steamkod)
+steamkod_f13 = random.choice(steamkod)
+
+# Steam Kod 8
+
+steamkod_g1 = random.choice(steamkod)
+steamkod_g2 = random.choice(steamkod)
+steamkod_g3 = random.choice(steamkod)
+steamkod_g4 = random.choice(steamkod)
+steamkod_g5 = random.choice(steamkod)
+
+steamkod_gbosluk = "-"
+
+steamkod_g6 = random.choice(steamkod)
+steamkod_g7 = random.choice(steamkod)
+steamkod_g8 = random.choice(steamkod)
+steamkod_g9 = random.choice(steamkod)
+
+steamkod_gbosluk2 = "-"
+
+steamkod_g10 = random.choice(steamkod)
+steamkod_g11 = random.choice(steamkod)
+steamkod_g12 = random.choice(steamkod)
+steamkod_g13 = random.choice(steamkod)
+
+
+steamkod_g1 = random.choice(steamkod)
+steamkod_g2 = random.choice(steamkod)
+steamkod_g3 = random.choice(steamkod)
+steamkod_g4 = random.choice(steamkod)
+steamkod_g5 = random.choice(steamkod)
+
+steamkod_gbosluk = "-"
+
+steamkod_g6 = random.choice(steamkod)
+steamkod_g7 = random.choice(steamkod)
+steamkod_g8 = random.choice(steamkod)
+steamkod_g9 = random.choice(steamkod)
+
+steamkod_gbosluk2 = "-"
+
+steamkod_g10 = random.choice(steamkod)
+steamkod_g11 = random.choice(steamkod)
+steamkod_g12 = random.choice(steamkod)
+steamkod_g13 = random.choice(steamkod)
+
+# Steam Kod 9
+
+steamkod_h1 = random.choice(steamkod)
+steamkod_h2 = random.choice(steamkod)
+steamkod_h3 = random.choice(steamkod)
+steamkod_h4 = random.choice(steamkod)
+steamkod_h5 = random.choice(steamkod)
+
+steamkod_hbosluk = "-"
+
+steamkod_h6 = random.choice(steamkod)
+steamkod_h7 = random.choice(steamkod)
+steamkod_h8 = random.choice(steamkod)
+steamkod_h9 = random.choice(steamkod)
+
+steamkod_hbosluk2 = "-"
+
+steamkod_h10 = random.choice(steamkod)
+steamkod_h11 = random.choice(steamkod)
+steamkod_h12 = random.choice(steamkod)
+steamkod_h13 = random.choice(steamkod)
+
+
+steamkod_h1 = random.choice(steamkod)
+steamkod_h2 = random.choice(steamkod)
+steamkod_h3 = random.choice(steamkod)
+steamkod_h4 = random.choice(steamkod)
+steamkod_h5 = random.choice(steamkod)
+
+steamkod_hbosluk = "-"
+
+steamkod_h6 = random.choice(steamkod)
+steamkod_h7 = random.choice(steamkod)
+steamkod_h8 = random.choice(steamkod)
+steamkod_h9 = random.choice(steamkod)
+
+steamkod_hbosluk2 = "-"
+
+steamkod_h10 = random.choice(steamkod)
+steamkod_h11 = random.choice(steamkod)
+steamkod_h12 = random.choice(steamkod)
+steamkod_h13 = random.choice(steamkod)
+
+# Steam Kod 10
+
+steamkod_i1 = random.choice(steamkod)
+steamkod_i2 = random.choice(steamkod)
+steamkod_i3 = random.choice(steamkod)
+steamkod_i4 = random.choice(steamkod)
+steamkod_i5 = random.choice(steamkod)
+
+steamkod_ibosluk = "-"
+
+steamkod_i6 = random.choice(steamkod)
+steamkod_i7 = random.choice(steamkod)
+steamkod_i8 = random.choice(steamkod)
+steamkod_i9 = random.choice(steamkod)
+
+steamkod_ibosluk2 = "-"
+
+steamkod_i10 = random.choice(steamkod)
+steamkod_i11 = random.choice(steamkod)
+steamkod_i12 = random.choice(steamkod)
+steamkod_i13 = random.choice(steamkod)
+
+
+steamkod_i1 = random.choice(steamkod)
+steamkod_i2 = random.choice(steamkod)
+steamkod_i3 = random.choice(steamkod)
+steamkod_i4 = random.choice(steamkod)
+steamkod_i5 = random.choice(steamkod)
+
+steamkod_ibosluk = "-"
+
+steamkod_i6 = random.choice(steamkod)
+steamkod_i7 = random.choice(steamkod)
+steamkod_i8 = random.choice(steamkod)
+steamkod_i9 = random.choice(steamkod)
+
+steamkod_ibosluk2 = "-"
+
+steamkod_i10 = random.choice(steamkod)
+steamkod_i11 = random.choice(steamkod)
+steamkod_i12 = random.choice(steamkod)
+steamkod_i13 = random.choice(steamkod)
+
+lorex = input("[1] - Programi Baslat \n[2] - Hakkinda \n[3] - Cikis")
+
+if lorex == '1':
+print("Steam Kod Olusturuluyor.")
+time.sleep(1)
+os.system("cls")
+print("Steam Kod Olusturuluyor..")
+time.sleep(1)
+os.system("cls")
+print("Steam Kod Olusturuluyor...")
+time.sleep(1)
+os.system("cls")
+print("Steam Kod Olusturuldu!")
+time.sleep(1)
+os.system("cls")
+
+print(steamkod1+steamkod2+steamkod3+steamkod4+steamkod5+steamkodbosluk+steamkod6+steamkod7+steamkod8+steamkod9+steamkodbosluk2+steamkod10+steamkod11+steamkod12+steamkod13)
+
+print(steamkod_a1+steamkod_a2+steamkod_a3+steamkod_a4+steamkod_a5+steamkod_abosluk+steamkod_a6+steamkod_a7+steamkod_a8+steamkod_a9+steamkod_abosluk2+steamkod_a10+steamkod_a11+steamkod_a12+steamkod_a13)
+
+print(steamkod_b1+steamkod_b2+steamkod_b3+steamkod_b4+steamkod_b5+steamkod_bbosluk+steamkod_b6+steamkod_b7+steamkod_b8+steamkod_b9+steamkod_bbosluk2+steamkod_b10+steamkod_b11+steamkod_b12+steamkod_b13)
+
+print(steamkod_c1+steamkod_c2+steamkod_c3+steamkod_c4+steamkod_c5+steamkod_cbosluk+steamkod_c6+steamkod_c7+steamkod_c8+steamkod_c9+steamkod_cbosluk2+steamkod_c10+steamkod_c11+steamkod_c12+steamkod_c13)
+
+print(steamkod_d1+steamkod_d2+steamkod_d3+steamkod_d4+steamkod_d5+steamkod_dbosluk+steamkod_d6+steamkod_d7+steamkod_d8+steamkod_d9+steamkod_dbosluk2+steamkod_d10+steamkod_d11+steamkod_d12+steamkod_d13)
+
+print(steamkod_e1+steamkod_e2+steamkod_e3+steamkod_e4+steamkod_e5+steamkod_ebosluk+steamkod_e6+steamkod_e7+steamkod_e8+steamkod_e9+steamkod_ebosluk2+steamkod_e10+steamkod_e11+steamkod_e12+steamkod_e13)
+
+print(steamkod_f1+steamkod_f2+steamkod_f3+steamkod_f4+steamkod_f5+steamkod_fbosluk+steamkod_f6+steamkod_f7+steamkod_f8+steamkod_f9+steamkod_fbosluk2+steamkod_f10+steamkod_f11+steamkod_f12+steamkod_f13)
+
+print(steamkod_g1+steamkod_g2+steamkod_g3+steamkod_g4+steamkod_g5+steamkod_gbosluk+steamkod_g6+steamkod_g7+steamkod_g8+steamkod_g9+steamkod_gbosluk2+steamkod_g10+steamkod_g11+steamkod_g12+steamkod_g13)
+
+
+print(steamkod_h1+steamkod_h2+steamkod_h3+steamkod_h4+steamkod_h5+steamkod_hbosluk+steamkod_h6+steamkod_h7+steamkod_h8+steamkod_h9+steamkod_hbosluk2+steamkod_h10+steamkod_h11+steamkod_h12+steamkod_h13)
+
+print(steamkod_i1+steamkod_i2+steamkod_i3+steamkod_i4+steamkod_i5+steamkod_ibosluk+steamkod_i6+steamkod_i7+steamkod_i8+steamkod_i9+steamkod_ibosluk2+steamkod_i10+steamkod_i11+steamkod_i12+steamkod_i13)
+print("Bu Ekran 100 saniye icinde kapanacak)
+time.sleep(100)
+if lorex == '2':
+print("\n                                  Coded By Lorex \n\n                           >> lorexofficiall.glitch.me <<")
+
+if lorex == '3':
+
+ time.sleep(1)
+
